9488e94be077ab47d89c347f138a93dda94ff6c9
red-one12 <radoanahmed1242000@gmail.com>
2025-02-06 10:44:57 +0600

    first commit

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+
+      <div className="min-h-screen">
+      <Outlet></Outlet>
+      </div>
+
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import myPhoto from '../assets/my photo/my photo.png'
+
+const Home = () => {
+  return (
+    <div>
+      hello form home
+      <img src={myPhoto} alt="" />
+    </div>
+  );
+};
+
+export default Home;
